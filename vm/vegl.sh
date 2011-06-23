50608a098135657c884cd6909139bbf75e2c0919
Josh Vote <Josh.Vote@csiro.au>
2011-06-23 02:49:16 +0000

    Added the basis for the vegl AMI worker shell script

---
+++ vm/vegl.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# ===============================================================
+#
+#
+#    Shell Script for managing the startup of the VEGL portal
+#
+#
+# ==============================================================
+
+#Start by waiting until the system starts up
+#TODO: Disabled for testing
+#sleep 30s
+
+#Next we want to download our user data string
+userDataHost=`route -n | awk '$4 ~ ".*G.*" {print $2}'`
+userDataString=`curl -L "http://$userDataHost:8773/latest/user-data"`
+
+#TODO: This is for testing purposes only
+userDataString='{"s3Bucket":"vegl-portal","s3BaseKeyPath":"test/input/","s3AccessKey":"AKIAJY4FBMSDLYAXH2MA","s3SecretKey":"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"}'
+
+s3Bucket=`echo $userDataString | jsawk 'return this.s3Bucket'`
+echo $s3Bucket
