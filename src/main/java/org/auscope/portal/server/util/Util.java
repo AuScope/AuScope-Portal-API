58b60ca5802ea14986053a07d356b18130bd332a
Josh Vote <Josh.Vote@csiro.au>
2011-07-04 04:44:14 +0000

    Updates to UTIL to support reverse parsing of DOM

--- src/main/java/org/auscope/portal/server/util/Util.java
+++ src/main/java/org/auscope/portal/server/util/Util.java
@@ -1,6 +1,7 @@
 package org.auscope.portal.server.util;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.springframework.stereotype.Repository;
@@ -8,8 +9,18 @@ import org.springframework.stereotype.Repository;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import java.io.StringReader;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Properties;
 
 /**
  * User: Mathew Wyatt
@@ -27,5 +38,32 @@ public class Util {
         Document doc = builder.parse(inputSource);
 
         return doc;
+    }
+    
+    /**
+     * Given a DOM (sub)tree generate a string representation with no formatting
+     * @param node The node to generate the XML for
+     * @param omitXmlDeclaration Whether the <?xml?> header should be omitted
+     * 
+     * @return
+     * @throws TransformerException
+     */
+    public String buildStringFromDom(Node node, boolean omitXmlDeclaration) throws TransformerException {
+    	StringWriter outText = new StringWriter();
+    	StreamResult sr = new StreamResult(outText);
+    	Properties oprops = new Properties();
+    	oprops.put(OutputKeys.METHOD, "xml");
+    	if (omitXmlDeclaration) {
+    		oprops.put(OutputKeys.OMIT_XML_DECLARATION, "yes");
+    	} else {
+    		oprops.put(OutputKeys.OMIT_XML_DECLARATION, "no");
+    	}
+    	TransformerFactory tf = TransformerFactory.newInstance();
+    	
+    	Transformer t = tf.newTransformer();
+		t.setOutputProperties(oprops);
+		t.transform(new DOMSource(node),sr);
+		
+		return outText.toString();
     }
 }
