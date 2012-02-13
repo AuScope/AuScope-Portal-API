f2d2ec45adbd1dea9d534a614e1f74162690ba39
Victor Tey <Victor.Tey@csiro.au>
2012-02-13 03:58:36 +0000

    clean up necessary comments from the sh files

--- vm/vegl.sh
+++ vm/vegl.sh
@@ -15,7 +15,7 @@ FINAL_SLEEP_LENGTH="15m"
 NTP_DATE_SERVER="pool.ntp.org"
 VEGL_WORKFLOW_VERSION="1"
 #cloud storage tool wrapper url
-WRAPPER_URL="http://vegl-portal.s3.amazonaws.com/vm/cloud.sh"
+WRAPPER_URL="https://svn.auscope.org/subversion/AuScopePortal/VEGL-Portal/trunk/vm/cloud.sh"
 export VEGL_SCRIPT_PATH="${WORKING_DIR}/vegl_script.py"
 export SUBSET_REQUEST_PATH="${WORKING_DIR}/subset_request.sh"
 ABORT_SHUTDOWN_PATH="${WORKING_DIR}/abort_shutdown"
