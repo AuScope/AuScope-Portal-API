1268ef030b2933cc0a47e6ec951b37bb2d485bb6
Josh Vote <Josh.Vote@csiro.au>
2012-12-07 04:21:45 +0000

    Added support for new Underworld lower boundary condition parameters

--- src/main/resources/org/auscope/portal/server/scriptbuilder/templates/underworld-gocad.py
+++ src/main/resources/org/auscope/portal/server/scriptbuilder/templates/underworld-gocad.py
@@ -1,6 +1,7 @@
 import subprocess, csv, math, os, sys, urllib, glob;
 import logging
 
+
 def cloudUpload(inFilePath, cloudKey):
     cloudBucket = os.environ["STORAGE_BUCKET"]
     cloudDir = os.environ["STORAGE_BASE_KEY_PATH"]
@@ -8,11 +9,25 @@ def cloudUpload(inFilePath, cloudKey):
     retcode = subprocess.call(["cloud", "upload", cloudKey, inFilePath, "--set-acl=public-read"])
     print "cloudUpload: " + inFilePath + " to " + queryPath + " returned " + str(retcode)
 
-print "starting"
-retcode = subprocess.call(["uwGocadGAMaterialsModelGenerator.sh", "--voxetFilename=${voxet-filename}", "--materialsPropName=${materials-property}", "--keyFilename=${voxet-key}", "--run"])
+#Build the default list of arguments
+argList = ["uwGocadGAMaterialsModelGenerator.sh", "--voxetFilename=${voxet-filename}", "--materialsPropName=${materials-property}", "--keyFilename=${voxet-key}", "--run"]
+
+#Add optional arguments
+lowerBoundaryFlux = "${lower-boundary-flux}"
+lowerBoundaryTemp = "${lower-boundary-temp}"
+if (len(lowerBoundaryFlux) > 0):
+    argList.append("--useLowerFluxBC")
+    argList.append("--lowerBoundaryFlux=" + lowerBoundaryFlux)
+
+if (len(lowerBoundaryTemp) > 0):
+    argList.append("--lowerBoundaryTemp=" + lowerBoundaryTemp)
+
+#Begin processing
+print argList
+retcode = subprocess.call(argList)
 print "result: " + str(retcode)
 
-
+#Upload results directory
 dirName = "output_GAGocadGeothermal/"
 print "Fetching output files"
 outputFiles =  glob.glob(dirName + "*")
--- src/main/webapp/js/ScriptBuilder/templates/UnderworldGocadTemplate.js
+++ src/main/webapp/js/ScriptBuilder/templates/UnderworldGocadTemplate.js
@@ -37,7 +37,7 @@ Ext.define('ScriptBuilder.templates.UnderworldGocadTemplate', {
         this._getTemplatedScriptGui(callback, 'underworld-gocad.py', {
             xtype : 'form',
             width : 400,
-            height : 200,
+            height : 300,
             items : [{
                 xtype : 'combo',
                 fieldLabel : 'Voxel Set',
@@ -71,9 +71,103 @@ Ext.define('ScriptBuilder.templates.UnderworldGocadTemplate', {
                 name : 'materials-property',
                 value : 'Geology',
                 allowBlank : false
+            },{
+                xtype : 'fieldset',
+                anchor : '100%',
+                checkboxToggle : true,
+                checkboxName : 'use-boundary-conditions',
+                title : 'Lower Boundary Conditions',
+                listeners : {
+                    collapse : this.onLowerBoundaryDisable,
+                    expand : Ext.bind(this.onLowerBoundaryEnable, this)
+                },
+                items : [{
+                    xtype : 'radiogroup',
+                    itemId : 'bc-radio',
+                    hideLabel : true,
+                    listeners : {
+                        change : this.onLowerBoundaryChange
+                    },
+                    items : [
+                        { boxLabel: 'Flux', inputValue: 'flux', name: 'lb-condition', checked: true },
+                        { boxLabel: 'Temperature', inputValue: 'temp', name: 'lb-condition' }]
+                },{
+                    xtype : 'numberfield',
+                    name : 'lower-boundary-flux',
+                    anchor : '100%',
+                    fieldLabel : 'Flux',
+                    itemId : 'lower-boundary-flux',
+                    decimalPrecision : 6,
+                    plugins: [{
+                        ptype: 'fieldhelptext',
+                        text: 'The lower boundary flux condition value.'
+                    }]
+                },{
+                    xtype : 'hiddenfield',
+                    name : 'lower-boundary-flux',
+                    itemId : 'lower-boundary-flux-empty',
+                    disabled : true,
+                    value : ''
+                },{
+                    xtype : 'numberfield',
+                    name : 'lower-boundary-temp',
+                    anchor : '100%',
+                    fieldLabel : 'Temperature',
+                    itemId : 'lower-boundary-temp',
+                    decimalPrecision : 6,
+                    hidden : true,
+                    disabled : true,
+                    plugins: [{
+                        ptype: 'fieldhelptext',
+                        text: 'The lower boundary temperature condition value.'
+                    }]
+                },{
+                    xtype : 'hiddenfield',
+                    name : 'lower-boundary-temp',
+                    itemId : 'lower-boundary-temp-empty',
+                    value : ''
+                }]
             }]
         });
-    }
+    },
 
+    onLowerBoundaryDisable : function(fieldSet) {
+        var flux = fieldSet.getComponent('lower-boundary-flux');
+        var fluxEmpty = fieldSet.getComponent('lower-boundary-flux-empty');
+        var temp = fieldSet.getComponent('lower-boundary-temp');
+        var tempEmpty = fieldSet.getComponent('lower-boundary-temp-empty');
+
+        temp.disable();
+        flux.disable();
+        tempEmpty.enable();
+        fluxEmpty.enable();
+    },
+
+    onLowerBoundaryEnable : function(fieldSet) {
+        var radioGroup = fieldSet.getComponent('bc-radio');
+        this.onLowerBoundaryChange(radioGroup, radioGroup.getValue());
+    },
+
+    onLowerBoundaryChange : function(radioGroup, newVal, oldVal) {
+        var flux = radioGroup.ownerCt.getComponent('lower-boundary-flux');
+        var fluxEmpty = radioGroup.ownerCt.getComponent('lower-boundary-flux-empty');
+        var temp = radioGroup.ownerCt.getComponent('lower-boundary-temp');
+        var tempEmpty = radioGroup.ownerCt.getComponent('lower-boundary-temp-empty');
+        if (newVal['lb-condition'] === 'flux') {
+            temp.hide();
+            temp.disable();
+            tempEmpty.enable();
+            flux.enable();
+            flux.show();
+            fluxEmpty.disable();
+        } else {
+            flux.hide();
+            flux.disable();
+            fluxEmpty.enable();
+            temp.enable();
+            temp.show();
+            tempEmpty.disable();
+        }
+    }
 });
 
