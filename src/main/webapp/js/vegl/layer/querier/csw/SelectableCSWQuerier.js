ffdeb0303688c772bfb5ab23b9bfeffc7ea6bb95
Richard Goh <Richard.Goh@csiro.au>
2013-02-12 08:50:28 +0000

    Added the missing edit functionality back to GOCAD online resources selection panel.

--- src/main/webapp/js/vegl/layer/querier/csw/SelectableCSWQuerier.js
+++ src/main/webapp/js/vegl/layer/querier/csw/SelectableCSWQuerier.js
@@ -66,17 +66,16 @@ Ext.define('vegl.layer.querier.csw.SelectableCSWQuerier', {
                         anchor : '100%',
                         value : cswRecord.get('contactOrg')
                     },{
-                        fieldLabel : 'Resources',
-                        xtype : 'onlineresourcepanel',
-                        cswRecords : cswRecord,
-                        selModel : selModel,
-                        sortable : false,
-                        hideHeaders : false
+                        xtype : 'dataselectionpanel',
+                        itemId : 'dataselection-panel',
+                        region : cswRecord.get('geographicElements'),
+                        cswRecords : [ cswRecord ]
                     },{
                         xtype : 'button',
                         margin : '5 0 0 0',
                         text : 'Capture selected',
-                        handler : Ext.bind(this.addSelectedResourcesToSession, this, selModel, true) 
+                        iconCls : 'add',
+                        handler : Ext.bind(this.addSelectedResourcesToSession, this) 
                     }]
                 }]
             }]
@@ -85,143 +84,20 @@ Ext.define('vegl.layer.querier.csw.SelectableCSWQuerier', {
         callback(this, [panel], queryTarget);
     },
     
-    addSelectedResourcesToSession : function(button, event, selModel) {        
-        // Check to ensure user has selected at least a file to capture
-        var selected = selModel.getSelection();
-        if (selected.length == 0) {
-            Ext.Msg.alert("Input error", "You haven't selected any file(s) to capture.");
-            return;
-        }
+    addSelectedResourcesToSession : function(button, event, selModel) {
+        var fieldSetContainer = button.findParentByType('fieldset');
+        var dataSelPanel = fieldSetContainer.getComponent('dataselection-panel');
         
-        // Array variables for holding user selected resources meta-data 
-        var urls = [];
-        var names = [];
-        var descriptions = [];
-        var localPaths = [];
-        var northBoundLatitudes = [];
-        var eastBoundLongitudes = [];
-        var southBoundLatitudes = [];
-        var westBoundLongitudes = [];
-        
-        // Populate the above variables by iterating thru user selected resources.
-        Ext.each(selected, function(orpRow) {
-            resource = orpRow.get('onlineResource');
-            cswRecord = orpRow.get('cswRecord');
-            
-            var bboxes = cswRecord.get('geographicElements');
-            var bbox = null;
-            if (bboxes.length > 0) {
-                bbox = bboxes[0];
-            }
-            
-            urls.push(resource.get('url') ? resource.get('url') : '');
-            names.push(resource.get('name') ? resource.get('name') : 'ERDDAP Subset Request');
-            descriptions.push(resource.get('description') ? resource.get('description') : '');
-            localPaths.push(resource.get('name') ? resource.get('name') : '/tmp/subset-request');
-            northBoundLatitudes.push(bbox.northBoundLatitude ? bbox.northBoundLatitude : 0);
-            eastBoundLongitudes.push(bbox.eastBoundLongitude ? bbox.eastBoundLongitude : 0);
-            southBoundLatitudes.push(bbox.southBoundLatitude ? bbox.southBoundLatitude : 0);
-            westBoundLongitudes.push(bbox.westBoundLongitude ? bbox.westBoundLongitude : 0);
-        });
-        
-        var loadMask = new Ext.LoadMask(Ext.getBody(), {
-            msg : 'Capturing input file(s)...',
-            removeMask : true
-        });
-        loadMask.show();
-        
-        Ext.Ajax.request({
-            url : 'addSelectedResourcesToSession.do',
-            params : {
-                url : urls, 
-                name : names,
-                description : descriptions,
-                localPath : localPaths,
-                northBoundLatitude : northBoundLatitudes,
-                eastBoundLongitude : eastBoundLongitudes,
-                southBoundLatitude : southBoundLatitudes,
-                westBoundLongitude : westBoundLongitudes
-            },
-            callback : function(options, success, response) {
-                loadMask.hide();
-                
-                if (!success) {
-                    Ext.Msg.alert('Communications Error', 'Failed to communicate with server. Please try again in a few minutes or report it to cg-admin@csiro.au.');
-                    return;
-                }
-
-                var responseObj = Ext.JSON.decode(response.responseText);
-                if (!responseObj.success) {
-                    Ext.Msg.alert('Application Error', 'An unexpected error has occurred. Please report it to cg-admin@csiro.au.');
-                    return;
-                }
-                
-                Ext.Msg.alert('Request Saved', 'Your download request has been saved. You can either continue selecting more data or <a href="jobbuilder.html">create a job</a> to process your existing selections.');
+        dataSelPanel.saveCurrentSelection(function(totalSelected, totalErrors) {
+            if (totalSelected === 0) {
+                Ext.Msg.alert('No selection', 'You haven\'t selected any data to capture. Please select one or more rows by checking the box alongside each row.');
+            } else if (totalErrors === 0) {
+                Ext.Msg.alert('Request Saved', 'Your ' + totalSelected + ' dataset(s) have been saved. You can either continue selecting more data or <a href="jobbuilder.html">create a job</a> to process your existing selections.');
+                this.close();
+            } else {
+                Ext.Msg.alert('Error saving data', 'There were one or more errors when saving some of the datasets you selected');
+                this.close();
             }
         });
-    },
-
-	/**
-     * This function is now deprecated. It has been replaced by
-	 * addSelectedResourcesToSession function.
-     */
-    showSelectionWindow : function(resource, cswRecord) {        
-        var bboxes = cswRecord.get('geographicElements');
-        var bbox = null;
-        if (bboxes.length > 0) {
-            bbox = bboxes[0];
-        }
-
-        var popup = Ext.create('Ext.window.Window', {
-            layout : 'fit',
-            width : 700,
-            height : 400,
-            modal : true,
-            title : 'Download from ' + cswRecord.get('name'),
-            items : [{
-                xtype : 'filedownloadpanel',
-                itemId : 'download-panel',
-                region : bbox,
-                url : resource.get('url'),
-                localPath : resource.get('name'),
-                name : resource.get('name'),
-                description : resource.get('description')
-            }],
-            buttons : [{
-                text : 'Capture File',
-                iconCls : 'add',
-                align : 'right',
-                handler : function(btn) {
-                    var parentWindow = btn.findParentByType('window');
-                    var panel = parentWindow.getComponent('download-panel');
-                    var params = panel.getForm().getValues();
-
-                    var myMask = new Ext.LoadMask(parentWindow, {msg:"Saving download request..."});
-                    myMask.show();
-                    Ext.Ajax.request({
-                        url : 'addDownloadRequestToSession.do',
-                        params : params,
-                        callback : function(options, success, response) {
-                            myMask.hide();
-                            if (!success) {
-                                portal.widgets.window.ErrorWindow.show('Communications Error', 'Unable to communicate with server. Please try again in a few minutes');
-                                return;
-                            }
-
-                            var responseObj = Ext.JSON.decode(response.responseText);
-                            if (!responseObj.success) {
-                                portal.widgets.window.ErrorWindow.show(responseObj);
-                                return;
-                            }
-
-                            parentWindow.close();
-                            Ext.Msg.alert('Request Saved', 'Your download request has been saved. You can either continue selecting more data or <a href="jobbuilder.html">create a job</a> to process your existing selections.');
-                        }
-                    });
-                }
-            }]
-        });
-
-        popup.show();
     }
 });
