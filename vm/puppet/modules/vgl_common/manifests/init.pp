035bbb6d8a85be476dc910d181a23d25951f5494
Florence Tan <Florence.Tan@csiro.au>
2014-02-19 08:25:37 +0000

    VGL-161 : Recreate AEM image based on the new upgarded CentOS6.5 image from NeCTAR

--- vm/puppet/modules/vgl_common/manifests/init.pp
+++ vm/puppet/modules/vgl_common/manifests/init.pp
@@ -4,17 +4,24 @@
 class vgl_common {
 
     # Install default packages
-    package { ["wget", "subversion", "mercurial", "ftp", "bzip2", "elfutils", "ntp", "ntpdate", "gcc", "gcc-c++", "make", "openssh", "openssh-clients", "swig", "libpng-devel", "freetype-devel", "atlas", "atlas-devel", "libffi-devel", "scipy", "python-keystoneclient"]: 
+    package { ["wget", "subversion", "mercurial", "ftp", "bzip2", "elfutils", "ntp", "ntpdate", "gcc", "gcc-c++", "make", "openssh", "openssh-clients", "swig", "libpng-devel", "freetype-devel", "atlas", "atlas-devel", "libffi-devel"]: 
         ensure => installed,
         require => Class["epel"],
     }
     
     # Install default pip packages
-    package {  ["numpy", "boto", "pyproj", "swift"]:
+    package {  ["numpy", "boto", "pyproj", "swift", "python-keystoneclient"]:
         ensure => installed,
         provider => "pip",
         require => Class["python_pip"],
     }  
+    package {["scipy"]:
+        ensure => installed,
+        provider => "pip",
+        require => [Class["python_pip"], Package["numpy"]],
+    }
+
+    
     
     # Install startup bootstrap
     $curl_cmd = "/usr/bin/curl"
