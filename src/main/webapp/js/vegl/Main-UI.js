8da073660af2f6614d5a57c9fb4f7afa6568b5cb
Florence Tan <Florence.Tan@csiro.au>
2014-05-22 02:21:14 +0000

    Fix broken UI due to changes in Portal Core (AUS-2478: Reduce steps in "apply filter" workflow)

--- src/main/webapp/js/vegl/Main-UI.js
+++ src/main/webapp/js/vegl/Main-UI.js
@@ -91,31 +91,37 @@ Ext.application({
         var layersPanel = Ext.create('portal.widgets.panel.LayerPanel', {
             id : 'vgl-layers-panel',
             title : 'Active Layers',
-            region : 'center',
+            region : 'south',
             store : layerStore,
             map : map,
+            height: 250,
+            split: true,
             allowDebugWindow : isDebugMode,
             listeners : {
-                //On selection, update our filter panel
-                select : function(rowModel, record, index) {
-                    filterPanel.showFilterForLayer(record);
-                },
                 removelayerrequest: function(sourceGrid, record) {
                     filterPanel.clearFilter();
                 }
             }
         });
 
+
+        var handleFilterSelectionComplete =  function(){
+            var activePanel = tabsPanel.activeTab;
+            activePanel.addSelectedLayerToActive();
+
+        };
+        
         /**
          * Used to show extra details for querying services
          */
         var filterPanel = Ext.create('portal.widgets.panel.FilterPanel', {
             id : 'vgl-filter-panel',
-            region: 'south',
+            region: 'center',
             layerPanel : layersPanel,
             map : map,
-            split: true,
-            height: 170
+            listeners : {
+                filterselectioncomplete : handleFilterSelectionComplete
+            }
         });
 
         var layerFactory = Ext.create('portal.layer.LayerFactory', {
@@ -139,10 +145,10 @@ Ext.application({
             }
 
             //Turn our KnownLayer/CSWRecord into an actual Layer
-            if (record instanceof portal.csw.CSWRecord) {
-                newLayer = layerFactory.generateLayerFromCSWRecord(record);
+            if (record instanceof portal.csw.CSWRecord) {               
+                newLayer = record.get('layer');
             } else {
-                newLayer = layerFactory.generateLayerFromKnownLayer(record);
+                newLayer = record.get('layer');
             }
 
             //We may need to show a popup window with copyright info
@@ -165,6 +171,18 @@ Ext.application({
             store : knownLayerStore,
             map : map,
             listeners : {
+                //On selection, update our filter panel
+                select : function(rowModel, record, index) {
+                    var newLayer;
+                    if(record.get('layer')){
+                        newLayer = record.get('layer');
+                    }else{
+                        newLayer = layerFactory.generateLayerFromKnownLayer(record);
+                        record.set('layer', newLayer);
+                    }
+
+                    filterPanel.showFilterForLayer(newLayer);
+                },
                 addlayerrequest : handleAddRecordToMap
             }
         });
@@ -191,7 +209,7 @@ Ext.application({
             //margins: '100 0 0 0',
             margins:'100 0 0 3',
             width: 350,
-            items:[tabsPanel , layersPanel, filterPanel]
+            items:[tabsPanel , filterPanel, layersPanel ]
         };
 
         /**
