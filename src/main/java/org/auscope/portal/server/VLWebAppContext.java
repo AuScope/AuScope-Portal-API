4623b1d8e6010712c746ab1b8ca8388aa5cf3117
joshvote <Josh.Vote@csiro.au>
2014-10-15 14:29:01 +0800

    Renamed some old vegl config files

--- src/main/java/org/auscope/portal/server/VLWebAppContext.java
+++ src/main/java/org/auscope/portal/server/VLWebAppContext.java
@@ -9,14 +9,14 @@ import org.auscope.portal.core.server.PortalProfileXmlWebApplicationContext;
  * @author vot002
  *
  */
-public class VeglWebAppContext extends PortalProfileXmlWebApplicationContext {
+public class VLWebAppContext extends PortalProfileXmlWebApplicationContext {
     @Override
     protected String[] getDefaultConfigLocations() {
         String[] locations = super.getDefaultConfigLocations();
 
         String[] auscopeLocations = Arrays.copyOf(locations, locations.length + 3);
-        auscopeLocations[auscopeLocations.length - 1] = DEFAULT_CONFIG_LOCATION_PREFIX + "vegl-known-layers.xml";
-        auscopeLocations[auscopeLocations.length - 2] = DEFAULT_CONFIG_LOCATION_PREFIX + "vegl-registries.xml";
+        auscopeLocations[auscopeLocations.length - 1] = DEFAULT_CONFIG_LOCATION_PREFIX + "vl-known-layers.xml";
+        auscopeLocations[auscopeLocations.length - 2] = DEFAULT_CONFIG_LOCATION_PREFIX + "vl-registries.xml";
         auscopeLocations[auscopeLocations.length - 3] = DEFAULT_CONFIG_LOCATION_PREFIX + "applicationContext-security.xml";
         return auscopeLocations;
     }
