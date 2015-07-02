5bde0adae2c56aae42d11949b8bff44105ddf181
joshvote <Josh.Vote@csiro.au>
2015-07-02 16:34:37 +0800

    Redid build by utilising --prefix

--- vm/puppet/modules/visit/manifests/init.pp
+++ vm/puppet/modules/visit/manifests/init.pp
@@ -1,17 +1,22 @@
-# Installs VisIt binaries and libraries: https://wci.llnl.gov/codes/visit
+# Builds VisIt binaries and libraries: https://wci.llnl.gov/codes/visit
+# Also installs a number of dependencies
 class visit {
 
-    package {["xutils-dev", "python-libxml2", "libglu-dev", "libglu1", "libglu1-mesa-dev", "libxt-dev"]:
+    package {["xutils-dev", "python-libxml2", "libglu-dev", "libglu1", "libglu1-mesa-dev", "libxt-dev", "libqt4-dev", "libqt4-opengl-dev"]:
         ensure => installed,
     }
     
+    # Copy across compiled visit libs
+    file { ["/usr/local/visit"]:
+        ensure => "directory",
+    }
 
     #Get build_visit script
     exec { "visit-dl":
         cwd => "/mnt",
         command => "/usr/bin/wget http://portal.nersc.gov/project/visit/releases/2.9.2/build_visit2_9_2",
         creates => "/mnt/build_visit2_9_2",
-        require => [Package["python-libxml2"], Package["xutils-dev"], Package["libglu-dev"], Package["libglu1"], Package["libglu1-mesa-dev"], Package["libxt-dev"]],
+        require => [File["/usr/local/visit"], Package["python-libxml2"], Package["xutils-dev"], Package["libglu-dev"], Package["libglu1"], Package["libglu1-mesa-dev"], Package["libxt-dev"], Package["libqt4-dev"], Package["libqt4-opengl-dev"]],
         timeout => 0,
     }
     
@@ -26,66 +31,15 @@ class visit {
     
     exec { "visit-build":
         cwd => "/mnt",
-        command => "/bin/bash build_visit_stripped --mesa --console --silo",
+        command => "/bin/bash build_visit_stripped --mesa --console --silo --system-python --system-qt --prefix /usr/local/visit",
         timeout => 0,
         require => Exec["visit-strip"],
     }
-    
-    file { ["/usr/local/visit", "/usr/local/visit/2.9.2"]:
-        ensure => "directory",
-        require => Exec["visit-build"],
-    }
-    
-    file { '/usr/local/visit/current':
-       ensure => 'link',
-       target => '2.9.2',
-       require => File["/usr/local/visit/2.9.2"],
-    }
-    
-    exec { "visit-install-lib":
-        cwd => "/mnt",
-        command => "/bin/cp -R /mnt/visit2.9.2/src/lib /usr/local/visit/current",
-        timeout => 0,
-        creates => "/usr/local/visit/current/lib",
-        require => File["/usr/local/visit/current"],
-    }
-    
-    exec { "visit-install-bin":
-        cwd => "/mnt",
-        command => "/bin/cp -R /mnt/visit2.9.2/src/bin /usr/local/visit/current",
-        timeout => 0,
-        creates => "/usr/local/visit/current/bin",
-        require => File["/usr/local/visit/current"],
-    }
-    
-    exec { "visit-install-exe":
-        cwd => "/mnt",
-        command => "/bin/cp -R /mnt/visit2.9.2/src/exe /usr/local/visit/current",
-        timeout => 0,
-        creates => "/usr/local/visit/current/exe",
-        require => File["/usr/local/visit/current"],
-    }
-    
-    exec { "visit-install-plugins":
-        cwd => "/mnt",
-        command => "/bin/cp -R /mnt/visit2.9.2/src/plugins /usr/local/visit/current",
-        timeout => 0,
-        creates => "/usr/local/visit/current/plugins",
-        require => File["/usr/local/visit/current"],
-    }
-    
-    exec { "visit-install-version":
-        cwd => "/mnt",
-        command => "/bin/cp /mnt/visit2.9.2/src/VERSION /usr/local/visit/current/VERSION",
-        timeout => 0,
-        creates => "/usr/local/visit/current/VERSION",
-        require => File["/usr/local/visit/current"],
-    }
-
 
     $visitShContent= '# Environment for visit
 export VISITINSTALL=/usr/local/visit/current
-export PATH=$VISITINSTALL/bin:$PATH
+export PATH=$VISITINSTALL/bin:/usr/local/vtk/6.1.0/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/visit/current/lib
 export PYTHONPATH=$VISITINSTALL/lib/site-packages:$PYTHONPATH
 '
     file {"visit-profile-env":
