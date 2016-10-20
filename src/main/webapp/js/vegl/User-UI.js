4a631762ff4fcd7f204a509a756cfef5d6b69da9
Stuart Woodman <stuart.woodman@csiro.au>
2016-10-20 14:20:04 +1100

    ANVGL 92 - Current AWS settings panel placed in a tab panel, still need to add NCI settings.

--- src/main/webapp/js/vegl/User-UI.js
+++ src/main/webapp/js/vegl/User-UI.js
@@ -34,72 +34,80 @@ Ext.application({
             style: {
                 'background-color': 'white'
             },
-            items: [{
+            items: [{	
                 xtype: 'box',
                 region: 'north',
                 applyTo: 'body',
                 height: 100
-            },{
+            },
+            {
                 region: 'center',
                 margin: '10 0 10 0',
                 border: false,
-                layout: 'center',
                 bodyStyle: {
                     'background-color': 'white'
                 },
                 items: [{
-                    xtype: 'panel',
-                    border: false,
-                    maxWidth: 1200,
-                    width: '100%',
-                    height: 500,
-                    layout: {
-                        type: 'hbox',
-                        pack: 'center'
-                    },
-                    items: [{
-                        xtype: 'panel',
-                        title: 'Step 1: Initialising your AWS',
-                        flex: 0.4,
-                        height: '100%',
-                        margin: '0 10 0 0',
-                        bodyStyle: {
-                            'background-color': 'white',
-                            padding: '5px',
-                        },
-                        html: '<h1>Tutorial Video</h1><img src="http://placehold.it/450x250"><p>You can also follow instructions at our wiki</p>',
-                        dockedItems: [{
-                            xtype: 'toolbar',
-                            dock: 'bottom',
-                            items: [{
-                                xtype: 'tbfill'
-                            },{
-                                xtype: 'button',
-                                scale: 'large',
-                                cls: 'important-button',
-                                text: 'Download Cloud Formation Policy',
-                                handler: function() {
-                                    portal.util.FileDownloader.downloadFile('secure/getCloudFormationScript.do');
-                                }
-                            }]
-                        }]
-                    },{
-                        xtype: 'panel',
-                        title: 'Step 2: Configuring ANVGL',
-                        flex: 0.6,
-                        height: '100%',
-                        margin: '0 0 0 10',
-                        bodyStyle: {
-                            'background-color': 'white'
-                        },
-                        layout: 'fit',
-                        items: [{
-                            border: false,
-                            xtype: 'userpanel'
-                        }]
-                    }]
+                	xtype: 'tabpanel',
+                	layout: 'fit',
+                	items: [{
+                		title: 'AWS',
+                		xtype: 'panel',
+	                    border: false,
+	                    maxWidth: 1200,
+	                    width: '100%',
+	                    height: 500,
+	                    margin: '10 10 10 10',
+	                    layout: {
+	                        type: 'hbox',
+	                        pack: 'center',
+	                    },
+	                    items: [{
+	                        xtype: 'panel',
+	                        title: 'Step 1: Initialising your AWS',
+	                        flex: 0.4,
+	                        height: '100%',
+	                        margin: '0 10 0 0',
+	                        bodyStyle: {
+	                            'background-color': 'white',
+	                            padding: '5px',
+	                        },
+	                        html: '<h1>Tutorial Video</h1><img src="http://placehold.it/450x250"><p>You can also follow instructions at our wiki</p>',
+	                        dockedItems: [{
+	                            xtype: 'toolbar',
+	                            dock: 'bottom',
+	                            items: [{
+	                                xtype: 'tbfill'
+	                            },{
+	                                xtype: 'button',
+	                                scale: 'large',
+	                                cls: 'important-button',
+	                                text: 'Download Cloud Formation Policy',
+	                                handler: function() {
+	                                    portal.util.FileDownloader.downloadFile('secure/getCloudFormationScript.do');
+	                                }
+	                            }]
+	                        }]
+	                    },{
+	                        xtype: 'panel',
+	                        title: 'Step 2: Configuring ANVGL',
+	                        flex: 0.6,
+	                        height: '100%',
+	                        margin: '0 0 0 10',
+	                        bodyStyle: {
+	                            'background-color': 'white'
+	                        },
+	                        layout: 'fit',
+	                        items: [{
+	                            border: false,
+	                            xtype: 'userpanel'
+	                        }]
+	                    }]
+                	},{
+                		title: 'NCI',
+                		html: 'Tab 2'
+                	}]
                 }]
-
             }],
             listeners: {
                 afterrender: function(vp) {
@@ -154,6 +162,7 @@ Ext.application({
                     });
                 }
             }
+            
         });
     }
 });
