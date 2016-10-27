40e1100c6225f37155c31db0235d69f6f50b49a7
joshvote <Josh.Vote@csiro.au>
2016-10-27 17:32:27 +0800

    Beginnings of Spring Security 4 support

--- src/main/java/org/auscope/portal/server/web/controllers/MenuController.java
+++ src/main/java/org/auscope/portal/server/web/controllers/MenuController.java
@@ -21,7 +21,7 @@ import org.apache.commons.logging.LogFactory;
 import org.auscope.portal.server.web.security.ANVGLUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
