a543d28acf8c43b67f4705e03527e8fb9c622473
Carsten Friedrich <Carsten.Friedrich@csiro.au>
2017-02-03 09:49:36 +1100

    Small adjustments to positioning and content

--- src/main/webapp/js/vegl/widgets/ANVGLFooter.js
+++ src/main/webapp/js/vegl/widgets/ANVGLFooter.js
@@ -5,6 +5,7 @@ Ext.define('vegl.widgets.ANVGLFooter', {
 	alias	: 'widget.ANVGLFooter',
 	extend	: 'Ext.panel.Panel',
 	height	: 90,
+	width	: 371,
 	html	: '<ul>'
         +     '<li><img height="90" width="90" id="img-csiro" src="img/logos/CSIRO_Grad_RGB_lr.jpg" alt="CSIRO" /></li>'
         +     '<li><img height="90" width="281" id="img-auscope" src="img/logos/auscope.jpg" alt="CSIRO" /></li>'
