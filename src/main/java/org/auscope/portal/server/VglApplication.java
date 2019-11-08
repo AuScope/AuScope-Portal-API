99a1959b007ef5da629ece2fc5f7e0aac9d69699
woo392 <stuart.woodman@csiro.au>
2019-11-08 15:27:35 +1100

    NOTE: This branch requires the similarly named branch in portal-core to work. * CSW search classes and associated tests moved to portal-core. * All portal-core components now pulled in via @ComponentScan in VglApplication. * NCI cloud service classes shifted to org.auscope.portal.server.web.service.cloud for consistency and to make scanning components easier. * Lots of historical cruft removed (.js, .css, .html).

--- src/main/java/org/auscope/portal/server/VglApplication.java
+++ src/main/java/org/auscope/portal/server/VglApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.auscope.portal.server", "org.auscope.portal.core"})
 public class VglApplication extends SpringBootServletInitializer {
 	
 	@Override
