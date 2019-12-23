3e83e1e80e4c559d52bd016cce1e03b5bdb1a26c
Bo Yan <bo.yan@csiro.au>
2019-12-23 13:35:13 +1100

    add a dummy implementation of HttpServletResponse. Somehow, mocking HttpServletResponse.getWriter generates exception.

--- src/test/java/org/auscope/portal/server/web/controllers/TestPurchaseController.java
+++ src/test/java/org/auscope/portal/server/web/controllers/TestPurchaseController.java
@@ -2,10 +2,16 @@ package org.auscope.portal.server.web.controllers;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.Locale;
 
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -20,7 +26,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import org.powermock.reflect.Whitebox;
-
 /**
  * Unit test for PurchaseController
  * @author Bo Yan
@@ -31,13 +36,12 @@ public class TestPurchaseController extends PortalTestClass {
     private SimpleWfsService mockWfsService = context.mock(SimpleWfsService.class);
     private CSWFilterService mockCSWFilterService = context.mock(CSWFilterService.class);
     private HttpServletRequest mockRequest = context.mock(HttpServletRequest.class);
-    private HttpServletResponse mockResponse = context.mock(HttpServletResponse.class);
     private ANVGLUserService mockUserService = context.mock(ANVGLUserService.class);
     
     private PurchaseController controller;
     
     final String serviceUrl = "http://example.org/service";
-
+    
     /**
      * Setup controller before all the tests
      */
@@ -63,7 +67,6 @@ public class TestPurchaseController extends PortalTestClass {
         final DelegatingServletInputStream input = new DelegatingServletInputStream(strStream);
 
         final ANVGLUser user = new ANVGLUser();
-        
         context.checking(new Expectations() {        	
             {
             	allowing(mockRequest).getInputStream(); will(returnValue(input));
@@ -72,7 +75,8 @@ public class TestPurchaseController extends PortalTestClass {
         });
         Whitebox.setInternalState(controller, "userService", mockUserService);
         
-    	controller.processDataPayment(mockRequest, mockResponse);
+        HttpServletResponse response = new HttpServletResponseImpl();
+    	controller.processDataPayment(mockRequest, response);
     }
     
     /**
@@ -96,6 +100,233 @@ public class TestPurchaseController extends PortalTestClass {
      */
     @Test
     public void testGetServiceType() {
+    	
+    }
+    
+    private class HttpServletResponseImpl implements HttpServletResponse {
+        private final StringWriter sw = new StringWriter();
+        private final PrintWriter pWriter = new PrintWriter(sw);
+
+		@Override
+		public String getCharacterEncoding() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getContentType() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ServletOutputStream getOutputStream() throws IOException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public PrintWriter getWriter() throws IOException {
+			return pWriter;
+		}
+
+		@Override
+		public void setCharacterEncoding(String charset) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setContentLength(int len) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setContentLengthLong(long length) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setContentType(String type) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setBufferSize(int size) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public int getBufferSize() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public void flushBuffer() throws IOException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void resetBuffer() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean isCommitted() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void reset() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setLocale(Locale loc) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public Locale getLocale() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void addCookie(Cookie cookie) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean containsHeader(String name) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public String encodeURL(String url) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String encodeRedirectURL(String url) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String encodeUrl(String url) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String encodeRedirectUrl(String url) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void sendError(int sc, String msg) throws IOException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void sendError(int sc) throws IOException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void sendRedirect(String location) throws IOException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setDateHeader(String name, long date) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void addDateHeader(String name, long date) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setHeader(String name, String value) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void addHeader(String name, String value) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setIntHeader(String name, int value) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void addIntHeader(String name, int value) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setStatus(int sc) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setStatus(int sc, String sm) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public int getStatus() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public String getHeader(String name) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Collection<String> getHeaders(String name) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Collection<String> getHeaderNames() {
+			// TODO Auto-generated method stub
+			return null;
+		}
     	
     }
     
