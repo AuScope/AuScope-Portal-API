776ace3a4636ffe3caaddf8a72c4b508696ff7fa
Jiang, Lingbo (Data61, North Ryde) <Lingbo.Jiang@csiro.au>
2020-09-16 21:56:11 +1000

    AUS-3458 Fixed IGSN-csiro filter issue.

--- src/main/java/org/auscope/portal/server/config/KnownLayers.java
+++ src/main/java/org/auscope/portal/server/config/KnownLayers.java
@@ -3120,7 +3120,7 @@ public class KnownLayers {
         layer.setOrder("50");
         // Optional filters
         List<AbstractBaseFilter> optionalFilters = new ArrayList<AbstractBaseFilter>();
-        UITextBox nameTextBox = new UITextBox("Identifier", "identifier", null, Predicate.ISLIKE);
+        UITextBox nameTextBox = new UITextBox("Identifier", "igsn", null, Predicate.ISLIKE);
         optionalFilters.add(nameTextBox);
         FilterCollection filterCollection = new FilterCollection();
         filterCollection.setOptionalFilters(optionalFilters);
