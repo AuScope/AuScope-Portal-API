d0f3ba70e3cfa3e56e931c01212648489a68a499
Carsten Friedrich <Carsten.Friedrich@csiro.au>
2021-08-20 13:19:01 +1000

    Removed obsolete layers as per AUS-3613 plus some additional ones.

--- src/main/java/org/auscope/portal/server/config/ProfilePortalTest.java
+++ src/main/java/org/auscope/portal/server/config/ProfilePortalTest.java
@@ -153,18 +153,6 @@ public class ProfilePortalTest {
 
     /* Generated from former test "Registered" tab START */
     @Autowired
-    KnownLayer knownTypeHighpSiteIronlayer;
-    @Autowired
-    KnownLayer knownTypeHighpfeaturetype;
-    @Autowired
-    KnownLayer knownTypeHighpReIronlayer;
-    @Autowired
-    KnownLayer knownTypeHighpSiteFeatureType;
-    @Autowired
-    KnownLayer knownTypeHighpSitePhoslayer;
-    @Autowired
-    KnownLayer knownTypeHighpRePhoslayer;
-    @Autowired
     KnownLayer knownTypeLandDamaPolyOfTasmMinResoTasm;
     @Autowired
     KnownLayer knownTypeNswDrillhole;
@@ -206,8 +194,6 @@ public class ProfilePortalTest {
     KnownLayer knownTypeCate1ExplLicePolyOfTasmMinResoTasm;
     @Autowired
     KnownLayer knownTypeGravBaseStatOfTasmMinResoTasm;
-    @Autowired
-    KnownLayer knownTypeRadnlOperR25PcprrL3Knmi;
     @Autowired
     KnownLayer knownTypeGsvGeologicalUnit250KAge;
     @Autowired
@@ -320,8 +306,6 @@ public class ProfilePortalTest {
     KnownLayer knownTypeHistoricalExplorationActivityLines;
     @Autowired
     KnownLayer knownTypeRestorations;
-    @Autowired
-    KnownLayer knownTypeBorehole;
     @Autowired
     KnownLayer knownTypeNswGeology;
     @Autowired
@@ -519,12 +503,6 @@ public class ProfilePortalTest {
         knownLayers.add(knownTypeSF0BoreholeNVCL);
 
         /* Generated from former test "Registered" tab START */
-        knownLayers.add(knownTypeHighpSiteIronlayer);
-        knownLayers.add(knownTypeHighpfeaturetype);
-        knownLayers.add(knownTypeHighpReIronlayer);
-        knownLayers.add(knownTypeHighpSiteFeatureType);
-        knownLayers.add(knownTypeHighpSitePhoslayer);
-        knownLayers.add(knownTypeHighpRePhoslayer);
         knownLayers.add(knownTypeLandDamaPolyOfTasmMinResoTasm);
         knownLayers.add(knownTypeNswDrillhole);
         knownLayers.add(knownTypeStrategicProspectivityZones);
@@ -546,7 +524,6 @@ public class ProfilePortalTest {
         knownLayers.add(knownTypeNswDrillholesPetroleum);
         knownLayers.add(knownTypeCate1ExplLicePolyOfTasmMinResoTasm);
         knownLayers.add(knownTypeGravBaseStatOfTasmMinResoTasm);
-        knownLayers.add(knownTypeRadnlOperR25PcprrL3Knmi);
         knownLayers.add(knownTypeGsvGeologicalUnit250KAge);
         knownLayers.add(knownTypeReleases);
         knownLayers.add(knownTypeMineOccuPoinOfTasmMinResoTasm);
@@ -605,7 +582,6 @@ public class ProfilePortalTest {
 
         knownLayers.add(knownTypeHistoricalExplorationActivityLines);
         knownLayers.add(knownTypeRestorations);
-        knownLayers.add(knownTypeBorehole);
         knownLayers.add(knownTypeNswGeology);
         knownLayers.add(knownTypeGeopIsosGravOverTmiRtpTilt);
         knownLayers.add(knownTypeGsvShearDisplacementStructure250K);
