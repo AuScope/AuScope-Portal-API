7f7325c255eef6afca0ca6291091a506ba8c5323
Vincent Fazio <vjf@users.noreply.github.com>
2022-05-04 13:01:41 +1000

    AUS-3516 Add "StateService" and sqlite db package

--- src/main/java/org/auscope/portal/server/config/AppContext.java
+++ src/main/java/org/auscope/portal/server/config/AppContext.java
@@ -58,6 +58,7 @@ import org.auscope.portal.core.view.ViewKnownLayerFactory;
 import org.auscope.portal.core.view.knownlayer.KnownLayer;
 import org.auscope.portal.core.xslt.GmlToHtml;
 import org.auscope.portal.core.xslt.WfsToKmlTransformer;
+import org.auscope.portal.core.services.admin.StateService;
 import org.auscope.portal.mscl.MSCLWFSService;
 import org.auscope.portal.server.vegl.VEGLJobManager;
 import org.auscope.portal.server.vegl.VGLJobStatusAndLogReader;
@@ -708,6 +709,10 @@ public class AppContext {
     @Bean
     public CatalogServicesHealthIndicator CatalogServicesHealthIndicator() {
         return new CatalogServicesHealthIndicator(cswCacheService(), cswKnownLayerService(), cswServiceList);
+
+    @Bean
+    public StateService stateServiceFactory() {
+        return new StateService(localCacheDir);
     }
 
 }
