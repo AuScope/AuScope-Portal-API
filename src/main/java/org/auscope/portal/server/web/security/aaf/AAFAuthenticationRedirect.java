35db020763abd708f04d92bde787e3eac97a8633
chrisvpeters <chris.peters@csiro.au>
2023-06-05 17:03:06 +1000

    AUS-3943

--- src/main/java/org/auscope/portal/server/web/security/aaf/AAFAuthenticationRedirect.java
+++ src/main/java/org/auscope/portal/server/web/security/aaf/AAFAuthenticationRedirect.java
@@ -1,11 +1,17 @@
 package org.auscope.portal.server.web.security.aaf;
 
+import java.net.URI;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -20,12 +26,10 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 @SecurityRequirement(name = "public")
 public class AAFAuthenticationRedirect {
 	
-	@Value("${spring.security.jwt.aaf.loginUrl}")
-	private String aafLoginUrl;
-	
-	@GetMapping("/login/aaf")
-	public String redirectToAAFLogin(HttpServletRequest request, HttpServletResponse response) {
-		return "redirect:" + aafLoginUrl;
-	}
-    
+    @Value("${spring.security.jwt.aaf.loginUrl}")
+    private String aafLoginUrl;
+	@GetMapping(value = "/login/aaf")
+    public ResponseEntity<Void> redirect(@RequestParam Map<String,String> input){
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(aafLoginUrl)).build();
+    }
 }
